d645a270ac7aacf9f12a0ab54c6a11fa11a0cce4
senuff1234 <akmaxmy2@gmail.com>
2025-10-26 11:22:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ function convertToBool(text, fault = "true") {
 }
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "O4w1mDKZ#ZfscVi3TniZj6uhlICzhFkLC4lAwXgT_xZFWezJT5k8",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:TtJesNveuNmqfYXbvAxYorkeFqTxQkKn@gondola.proxy.rlwy.net:52279",
   OWNER_NUM: process.env.OWNER_NUM || "94704206946",
 };
